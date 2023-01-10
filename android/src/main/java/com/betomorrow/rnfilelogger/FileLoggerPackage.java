1999149d1dcdc4a169f8eae388e159fa4849a4c2
Fabien Drault <fdrault@betomorrow.com>
2023-01-10 15:23:09 +0100

    Configure to support new architecture on Android

--- android/src/main/java/com/betomorrow/rnfilelogger/FileLoggerPackage.java
+++ android/src/main/java/com/betomorrow/rnfilelogger/FileLoggerPackage.java
@@ -1,23 +1,47 @@
 package com.betomorrow.rnfilelogger;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import androidx.annotation.Nullable;
 
-import com.facebook.react.ReactPackage;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.module.model.ReactModuleInfo;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
 
-public class FileLoggerPackage implements ReactPackage {
+public class FileLoggerPackage extends TurboReactPackage {
+
+    @Nullable
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new FileLoggerModule(reactContext));
+    public NativeModule getModule(String name, ReactApplicationContext reactApplicationContext) {
+        if (name.equals("RNFileLogger")) {
+            return new FileLoggerModule(reactApplicationContext);
+        } else {
+            return null;
+        }
     }
-
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+    public ReactModuleInfoProvider getReactModuleInfoProvider() {
+        return new ReactModuleInfoProvider() {
+            @Override
+            public Map<String, ReactModuleInfo> getReactModuleInfos() {
+                final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+                boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+                moduleInfos.put(
+                        "RNFileLogger",
+                        new ReactModuleInfo(
+                                "RNFileLogger",
+                                "RNFileLogger",
+                                false, // canOverrideExistingModule
+                                false, // needsEagerInit
+                                true, // hasConstants
+                                false, // isCxxModule
+                                isTurboModule // isTurboModule
+                        ));
+                return moduleInfos;
+            }
+        };
     }
 }
---
+++ src/NativeRNFileLogger.ts
@@ -0,0 +1,25 @@
+import type { TurboModule } from "react-native";
+import { TurboModuleRegistry } from "react-native";
+
+export type NativeConfigureOptions = {
+	dailyRolling: boolean;
+	maximumFileSize: number;
+	maximumNumberOfFiles: number;
+	logsDirectory?: string;
+};
+
+export type SendByEmailOptions = {
+	to?: string[];
+	subject?: string;
+	body?: string;
+};
+
+export interface Spec extends TurboModule {
+	configure(options: NativeConfigureOptions): Promise<void>;
+	write(level: number, msg: string): void;
+	getLogFilePaths(): Promise<string[]>;
+	deleteLogFiles(): Promise<void>;
+	sendLogFilesByEmail(options: SendByEmailOptions): Promise<void>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>("RNFileLogger");
