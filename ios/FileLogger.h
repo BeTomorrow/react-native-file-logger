92002475a86c0862d41c3d48db2e019ae4fef345
Fabien Drault <fdrault@betomorrow.com>
2023-01-18 16:56:38 +0100

    Add backward compatibility on iOS

--- ios/FileLogger.h
+++ ios/FileLogger.h
@@ -1,5 +1,11 @@
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "FileLoggerSpec.h"
+
+@interface FileLogger : NSObject <NativeFileLoggerSpec>
+#else
 #import <React/RCTBridgeModule.h>
 
 @interface FileLogger : NSObject <RCTBridgeModule>
+#endif
 
 @end
