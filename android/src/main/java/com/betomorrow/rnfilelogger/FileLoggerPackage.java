df03f60847f51e4194f65f93bd5fb424df56ebf4
Fabien Drault <fdrault@betomorrow.com>
2023-01-18 15:26:32 +0100

    Add backward compatibility on Android

--- android/src/main/java/com/betomorrow/rnfilelogger/FileLoggerPackage.java
+++ android/src/main/java/com/betomorrow/rnfilelogger/FileLoggerPackage.java
@@ -16,32 +16,29 @@ public class FileLoggerPackage extends TurboReactPackage {
     @Nullable
     @Override
     public NativeModule getModule(String name, ReactApplicationContext reactApplicationContext) {
-        if (name.equals("FileLogger")) {
-            return new FileLoggerModule(reactApplicationContext);
+        if (name.equals(FileLoggerModule.NAME)) {
+            return (NativeModule) new FileLoggerModule(reactApplicationContext);
         } else {
             return null;
         }
     }
     @Override
     public ReactModuleInfoProvider getReactModuleInfoProvider() {
-        return new ReactModuleInfoProvider() {
-            @Override
-            public Map<String, ReactModuleInfo> getReactModuleInfos() {
-                final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
-                boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-                moduleInfos.put(
-                        "FileLogger",
-                        new ReactModuleInfo(
-                                "FileLogger",
-                                "FileLogger",
-                                false, // canOverrideExistingModule
-                                false, // needsEagerInit
-                                true, // hasConstants
-                                false, // isCxxModule
-                                isTurboModule // isTurboModule
-                        ));
-                return moduleInfos;
-            }
+        return () -> {
+            final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+            boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+            moduleInfos.put(
+                    FileLoggerModule.NAME,
+                    new ReactModuleInfo(
+                            FileLoggerModule.NAME,
+                            FileLoggerModule.NAME,
+                            false, // canOverrideExistingModule
+                            false, // needsEagerInit
+                            true, // hasConstants
+                            false, // isCxxModule
+                            isTurboModule // isTurboModule
+                    ));
+            return moduleInfos;
         };
     }
 }
---
+++ android/src/turbo/FileLoggerSpec.java
@@ -0,0 +1,10 @@
+package com.betomorrow.rnfilelogger;
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class FileLoggerSpec extends NativeFileLoggerSpec {
+
+    FileLoggerSpec(ReactApplicationContext context) {
+        super(context);
+    }
+
+}
