a13bf76723275e6b4c6cafd770cd9e563359d108
Aloïs Guichard <aguichard@betomorrow.com>
2023-11-06 11:35:20 +0100

    Update example app

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,40 +1,11 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * with multiple workarounds for this issue with symlinks:
- * https://github.com/facebook/metro/issues/1
- *
- * with thanks to @johnryan (<https://github.com/johnryan>)
- * for the pointers to multiple workaround solutions here:
- * https://github.com/facebook/metro/issues/1#issuecomment-541642857
- *
- * see also this discussion:
- * https://github.com/brodybits/create-react-native-module/issues/232
- */
-const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => {
-          return path.join(__dirname, `node_modules/${name}`);
-        },
-      },
-    ),
-  },
-  watchFolders: [
-    path.resolve(__dirname, '..'),
-    path.resolve(__dirname, '../node_modules'),
-  ],
-};
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
