61120d0653551727afc764da5940c59e574d27df
周颖飞 <zhouyingfei@gofoexpress.com>
2025-07-17 09:49:34 +0800

    fix: Fix the import error

--- ios/Log.h
+++
@@ -1,8 +0,0 @@
-// Log.h
-#import "CocoaLumberjack.h"
-
-@interface CustomLogFileManager : DDLogFileManagerDefault
-
-- (instancetype)initWithLogsDirectory:(NSString *)logsDirectory fileName:(NSString *)name;
-
-@end
